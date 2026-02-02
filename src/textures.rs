14f6248c0603611313e4e0ac1ffc8ace4c0e960c
Haksell <axel.brisse@gmail.com>
2026-02-02 13:46:40 +0100

    OnceCell -> OnceLock

--- src/textures.rs
+++ src/textures.rs
@@ -1,12 +1,14 @@
-use nannou::{wgpu::Texture, App};
-use once_cell::sync::OnceCell;
-use std::{path::Path, sync::Mutex};
+use nannou::{App, wgpu::Texture};
+use std::{
+    path::Path,
+    sync::{Mutex, OnceLock},
+};
 
-pub static TEXTURE_BACKGROUND: OnceCell<Mutex<Texture>> = OnceCell::new();
-pub static TEXTURE_BLACK: OnceCell<Mutex<Texture>> = OnceCell::new();
-pub static TEXTURE_WHITE: OnceCell<Mutex<Texture>> = OnceCell::new();
+pub static TEXTURE_BACKGROUND: OnceLock<Mutex<Texture>> = OnceLock::new();
+pub static TEXTURE_BLACK: OnceLock<Mutex<Texture>> = OnceLock::new();
+pub static TEXTURE_WHITE: OnceLock<Mutex<Texture>> = OnceLock::new();
 
-const TEXTURES_DIRECTORY: &'static str = "assets";
+const TEXTURES_DIRECTORY: &str = "assets";
 
 fn handle_error<T, E: std::fmt::Debug>(result: Result<T, E>) -> T {
     result.unwrap_or_else(|err| {
@@ -15,7 +17,7 @@ fn handle_error<T, E: std::fmt::Debug>(result: Result<T, E>) -> T {
     })
 }
 
-fn init_texture(app: &App, texture_background: &OnceCell<Mutex<Texture>>, path: &str) {
+fn init_texture(app: &App, texture_background: &OnceLock<Mutex<Texture>>, path: &str) {
     let texture_path = Path::new(TEXTURES_DIRECTORY).join(path);
     let texture = handle_error(Texture::from_path(app, texture_path));
     handle_error(texture_background.set(Mutex::new(texture)));
