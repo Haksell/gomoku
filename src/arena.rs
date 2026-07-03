169d4c94f7a3efd3e27caf1e9c2fdf584dc714f2
Haksell <axel.brisse@gmail.com>
2026-07-03 16:15:39 +0200

    arena swap
    Co-authored-by: solvictor <victor.egret.pro@gmail.com>

--- src/arena.rs
+++ src/arena.rs
@@ -10,17 +10,25 @@ pub fn run(args: &Args) {
         "`num_games` is reserved for bot vs bot matches."
     );
 
-    let mut black_wins = 0.;
+    let mut player1_wins = 0.;
     for game_idx in 1..=args.num_games {
-        let mut game = Game::new(args.black_player, args.white_player);
+        let (player1_color, black, white) = match game_idx & 1 {
+            0 => (PlayerColor::White, args.white_player, args.black_player),
+            1 => (PlayerColor::Black, args.black_player, args.white_player),
+            _ => unreachable!(),
+        };
+
+        let mut game = Game::new(black, white);
         game.play_game();
+
         match game.state {
             GameState::Playing => unreachable!(),
-            GameState::Won(PlayerColor::Black) => black_wins += 1.,
-            GameState::Draw => black_wins += 0.5,
-            GameState::Won(PlayerColor::White) => {}
+            GameState::Draw => player1_wins += 0.5,
+            GameState::Won(winner) if winner == player1_color => player1_wins += 1.,
+            GameState::Won(_) => {}
         }
-        let black_percentage = 100. * black_wins / game_idx as f64;
-        println!("Black won {black_wins}/{game_idx} games ({black_percentage:.1}%)");
+
+        let player1_percentage = 100. * player1_wins / game_idx as f64;
+        println!("Player 1 won {player1_wins}/{game_idx} games ({player1_percentage:.1}%)");
     }
 }
