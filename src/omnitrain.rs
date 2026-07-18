8d235ba9f93f6e9256a8a8b6ea560e22f44b7862
Haksell <axel.brisse@gmail.com>
2026-07-18 20:34:06 +0200

    wip incdec multiple

--- src/omnitrain.rs
+++ src/omnitrain.rs
@@ -17,6 +17,7 @@ use nannou::rand::{Rng as _, thread_rng};
 use rayon::iter::{IntoParallelIterator as _, ParallelIterator as _};
 use std::{
     array,
+    iter::repeat_with,
     sync::{
         Arc, Mutex,
         atomic::{AtomicU32, Ordering},
@@ -24,35 +25,59 @@ use std::{
 };
 
 const N_MUTATIONS: usize = UNIQUE_STENCIL_INDICES + 9;
+const PAIRS_BY_EPOCH: usize = 5;
 
 pub fn run() {
     let epoch = AtomicU32::default();
     let best_coeffs = Arc::new(Mutex::new(INITIAL_COEFFS.clone()));
 
+    // TODO: find a cleaner rayon infinite loop
     (0..u128::MAX).into_par_iter().for_each(|_| {
-        let mut coeffs1 = best_coeffs.lock().unwrap().clone();
-        let mut coeffs2 = coeffs1.clone();
-        let updates1: [i64; N_MUTATIONS] =
-            array::from_fn(|_| if thread_rng().gen_ratio(1, 2) { 1 } else { -1 });
-        for (i, &update1) in updates1.iter().enumerate() {
-            update_coeffs(&mut coeffs1, i, update1);
-            update_coeffs(&mut coeffs2, i, -update1);
-        }
-
-        let player1 = Player::Bot {
-            bot: idabp,
-            heuristic: Heuristic { fun: coeffistic, coeffs: Some(coeffs1) },
-        };
-        let player2 = Player::Bot {
-            bot: idabp,
-            heuristic: Heuristic { fun: coeffistic, coeffs: Some(coeffs2) },
+        let should_inc2: [[bool; N_MUTATIONS]; PAIRS_BY_EPOCH] = {
+            let mut should_inc2 = [[false; N_MUTATIONS]; PAIRS_BY_EPOCH];
+            let mut rng = thread_rng();
+            for i in 0..N_MUTATIONS {
+                let mut remaining_increments = PAIRS_BY_EPOCH / 2;
+                for j in 0..PAIRS_BY_EPOCH {
+                    let inc2 =
+                        rng.gen_ratio(remaining_increments as u32, (PAIRS_BY_EPOCH - j) as u32);
+                    should_inc2[j][i] = inc2;
+                    remaining_increments -= inc2 as usize;
+                }
+            }
+            should_inc2
         };
 
-        let wins2 = play_pair(&player1, &player2);
-        if wins2 != 1 {
+        let wins2 = (0..PAIRS_BY_EPOCH)
+            .map(|i| {
+                let mut coeffs1 = best_coeffs.lock().unwrap().clone();
+                let mut coeffs2 = coeffs1.clone();
+                let updates1: [i64; N_MUTATIONS] =
+                    array::from_fn(|_| if thread_rng().gen_ratio(1, 2) { 1 } else { -1 });
+                for (i, &update1) in updates1.iter().enumerate() {
+                    update_coeffs(&mut coeffs1, i, update1);
+                    update_coeffs(&mut coeffs2, i, -update1);
+                }
+                let player1 = Player::Bot {
+                    bot: idabp,
+                    heuristic: Heuristic { fun: coeffistic, coeffs: Some(coeffs1) },
+                };
+                let player2 = Player::Bot {
+                    bot: idabp,
+                    heuristic: Heuristic { fun: coeffistic, coeffs: Some(coeffs2) },
+                };
+                play_pair(&player1, &player2)
+            })
+            .sum::<u32>() as usize;
+
+        if wins2 != PAIRS_BY_EPOCH {
             let mut best_coeffs = best_coeffs.lock().unwrap();
             for (i, &update1) in updates1.iter().enumerate() {
-                update_coeffs(&mut best_coeffs, i, if wins2 == 0 { update1 } else { -update1 });
+                update_coeffs(
+                    &mut best_coeffs,
+                    i,
+                    if wins2 < PAIRS_BY_EPOCH { update1 } else { -update1 },
+                );
             }
         }
 
