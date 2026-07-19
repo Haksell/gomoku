df2aaf1ef85ed1bec7c445578f43269edebe37b0
Haksell <axel.brisse@gmail.com>
2026-07-19 04:33:48 +0200

    LEARNING_RATE_EXP instead of implicit 0.5

--- src/omnitrain.rs
+++ src/omnitrain.rs
@@ -24,6 +24,7 @@ use std::{
 };
 
 const N_MUTATIONS: usize = UNIQUE_STENCIL_INDICES + 9;
+const LEARNING_RATE_EXP: f64 = 0.6;
 
 pub fn run() {
     let epoch = AtomicU32::default();
@@ -32,7 +33,7 @@ pub fn run() {
     // TODO: find a cleaner rayon infinite loop
     rayon::iter::repeat(()).for_each(|()| {
         let max_abs_coeff = best_coeffs.lock().unwrap().iter().map(|x| x.abs()).max().unwrap();
-        let learning_rate = ((max_abs_coeff as f64).sqrt().ceil() as i64).max(1);
+        let learning_rate = ((max_abs_coeff as f64).powf(LEARNING_RATE_EXP).ceil() as i64).max(1);
 
         let update_sums = (0..learning_rate)
             .map(|_| {
@@ -63,7 +64,8 @@ pub fn run() {
                     _ => unreachable!(),
                 }
             })
-            .fold([0; N_MUTATIONS], |acc, res| array::from_fn(|i| acc[i] + res[i]));
+            .reduce(|acc, res| array::from_fn(|i| acc[i] + res[i]))
+            .unwrap();
 
         {
             let mut best_coeffs = best_coeffs.lock().unwrap();
